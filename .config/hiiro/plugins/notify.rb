3ceee560c1eb7f53ee38fe44ea0ed38aa96531b2
Joshua Toyota <jearsh+github@gmail.com>
2025-11-21 19:31:56 -0500

    add notify plugin

---
+++ .config/hiiro/plugins/notify.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+module Notify
+  def self.load(hiiro)
+    hiiro.log "Plugin loaded: #{name}"
+
+    hiiro.load_plugin(Tmux)
+    attach_methods(hiiro)
+    add_subcommands(hiiro)
+  end
+
+  def self.add_subcommands(hiiro)
+    hiiro.add_subcmd(:notify) do |message, link=nil, title=nil|
+      hiiro.notify(message, title:, link:)
+    end
+  end
+
+  def self.attach_methods(hiiro)
+    hiiro.instance_eval do
+      def notify(message, title: nil, link: nil)
+        args = ['terminal-notifier', '-message', message]
+        args += ['-title', title] if title
+        args += ['-open', link] if link
+
+        system(*args) if system('which', 'terminal-notifier')
+      end
+    end
+  end
+end
